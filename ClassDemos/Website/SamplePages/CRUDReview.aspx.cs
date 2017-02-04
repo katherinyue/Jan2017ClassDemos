35906ac233f51c3047127495124e9a0355230dfc
Tongyue <tongyue94@gmail.com>
2017-02-03 18:53:59 -0700

    Coded Crud: controler command review and web page implementation
    add/ update/delete

--- ClassDemos/Website/SamplePages/CRUDReview.aspx.cs
+++ ClassDemos/Website/SamplePages/CRUDReview.aspx.cs
@@ -64,22 +64,107 @@ public partial class SamplePages_CRUDReview : System.Web.UI.Page
 
     protected void AlbumList_SelectedIndexChanged(object sender, EventArgs e)
     {
-       
+        //coming from tab Find
+        GridViewRow agvrow = AlbumList.Rows[AlbumList.SelectedIndex];
+        string albumid = (agvrow.FindControl("AlbumID") as Label).Text;
+        string albumtitle = (agvrow.FindControl("Title") as Label).Text;
+        string albumyear = (agvrow.FindControl("Year") as Label).Text;
+        string albumlabel = (agvrow.FindControl("AlbumLabel") as Label).Text;
+        string artistid = (agvrow.FindControl("ArtistID") as Label).Text;
+
+        //displayint on tab find
+        SelectedTitle.Text = albumtitle + "release in " + albumyear + " by " + albumlabel;
+        //filling controls on tab Maintain
+        AlbumID.Text = albumid;
+        AlbumTitle.Text = albumtitle;
+        ArtistList.SelectedValue = artistid;
+        AlbumReleaseYear.Text = albumyear;
+        AlbumReleaseLabel.Text = albumlabel;
+
 
     }
 
     protected void AddAlbum_Click(object sender, EventArgs e)
     {
-       
+        //retest the validation of the incoming data via the Validation Controls
+       if (IsValid)
+        {
+            //any other business rules
+            MessageUserControl2.TryRun(() =>
+            {
+                AlbumController sysmgr = new AlbumController();
+                Album newalbum = new Album();
+                newalbum.Title = AlbumTitle.Text;
+                newalbum.ArtistId = int.Parse(ArtistList.SelectedValue);
+                newalbum.ReleaseYear = int.Parse(AlbumReleaseYear.Text);
+                newalbum.ReleaseLabel = string.IsNullOrEmpty(AlbumReleaseLabel.Text) ? null : AlbumReleaseYear.Text;
+
+                sysmgr.Albums_Add(newalbum);
+            },"Add Album", "Album has been successfully added to the database.");
+        }
        
     }
     protected void UpdateAlbum_Click(object sender, EventArgs e)
     {
-       
+        if (IsValid)
+        {
+            //any other business rules
+            if (string.IsNullOrEmpty(AlbumID.Text))
+            {
+                MessageUserControl2.ShowInfo("Missing Data", "Missing Album Id. Use Find to locate the album you wish to maintain.");
+            }
+            else
+            {
+                int albumid = 0;
+                if(int.TryParse(AlbumID.Text, out albumid))
+                {
+                    MessageUserControl2.TryRun(() =>
+                    {
+                        AlbumController sysmgr = new AlbumController();
+                        Album newalbum = new Album();
+                        newalbum.AlbumId = albumid;
+                        newalbum.Title = AlbumTitle.Text;
+                        newalbum.ArtistId = int.Parse(ArtistList.SelectedValue);
+                        newalbum.ReleaseYear = int.Parse(AlbumReleaseYear.Text);
+                        newalbum.ReleaseLabel = string.IsNullOrEmpty(AlbumReleaseLabel.Text) ? null : AlbumReleaseYear.Text;
+
+                        sysmgr.Albums_Update(newalbum);
+                    }, "Update Album", "Album has been successfully update on the database.");
+                }
+                else
+                {
+                    MessageUserControl2.ShowInfo("Invalid Data", "Album Id. Use Find to locate the album you wish to maintain.");
+                }               
+            }           
+        }
     }
     protected void DeleteAlbum_Click(object sender, EventArgs e)
     {
        
+            //any other business rules
+            if (string.IsNullOrEmpty(AlbumID.Text))
+            {
+                MessageUserControl2.ShowInfo("Missing Data", "Missing Album Id. Use Find to locate the album you wish to maintain.");
+            }
+            else
+            {
+                int albumid = 0;
+                if (int.TryParse(AlbumID.Text, out albumid))
+                {
+                    MessageUserControl2.TryRun(() =>
+                    {
+                        AlbumController sysmgr = new AlbumController();
+                        
+                  
+                        sysmgr.Albums_Delete(albumid);
+                    }, "Delete Album", "Album has been successfully delete from the database.");
+                }
+                else
+                {
+                    MessageUserControl2.ShowInfo("Invalid Data", "Album Id. Use Find to locate the album you wish to maintain.");
+                }
+            }
+        
     }
     protected void Clear_Click(object sender, EventArgs e)
     {
